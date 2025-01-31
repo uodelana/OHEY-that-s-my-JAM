deaf9510487ea1514928c220962d1ecd3891a2cd
Yves Tago <tago@ualberta.ca>
2025-01-30 17:20:35 -0700

    Resolved Merge Conflict

---
+++ app/src/main/java/com/uodelana/oheythatsmyjam/Circle.java
@@ -0,0 +1,19 @@
+public class Circle extends Shape {
+
+    private String color = "blue";  // as per lab-4
+
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ app/src/main/java/com/uodelana/oheythatsmyjam/Shape.java
@@ -0,0 +1,8 @@
+package com.uodelana.oheythatsmyjam;
+
+public abstract class Shape {
+    private String color = "red"; // as per lab-4
+
+    private int x;
+    private int y;
+}
